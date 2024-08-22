0b0c6fb46ee846b99365fea3491fc3b9faa7506d
Phan Kien <91230993+kienphan1234@users.noreply.github.com>
2024-08-22 11:02:47 +0700

    update css table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CatalogueComponent } from './catalogue/catalogue.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/catalogue', pathMatch: 'full' },
     {
         path: 'catalogue',
         component: CatalogueComponent
