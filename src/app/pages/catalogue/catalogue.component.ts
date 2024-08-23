849420ff924a4932c2325c67902c944220d7060d
Phan Kien <91230993+kienphan1234@users.noreply.github.com>
2024-08-23 12:40:38 +0700

    update select color and size

--- src/app/pages/catalogue/catalogue.component.ts
+++ src/app/pages/catalogue/catalogue.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
-import { BaseSku, catalogue, ProductData } from '../../core/models/catalogue';
+import { BaseSku, catalogue, Option, OptionValue, ProductData } from '../../core/models/catalogue';
 import { CatalogueService } from '../../core/services/catalogue.service';
 import { CommonModule } from '@angular/common';
 import { UniqueLocationPipe } from "../../core/pipe/unique-location.pipe";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-catalogue',
   standalone: true,
-  imports: [CommonModule, UniqueLocationPipe],
+  imports: [CommonModule, UniqueLocationPipe,FormsModule],
   templateUrl: './catalogue.component.html',
   styleUrl: './catalogue.component.css'
 })
 export class CatalogueComponent {
   productData: ProductData | undefined;
-;
-
+  sizeOptions: any[] = [];
+  sizeOptions1: any[] = [];
+  filteredList: any[] = [];
+  selectedColor: string | null = "";
+  selectedSize: string | null = "";
   constructor(private productService: CatalogueService) { }
 
   ngOnInit(): void {
@@ -24,10 +28,45 @@ export class CatalogueComponent {
   loadProductData(): void {
     this.productService.getProductData().subscribe({
       next: (response: catalogue) => {
-        this.productData = response.data;
-        
+        this.productData = response.data; 
+        this.sizeOptions = this.productData?.options.filter(option => option.name === 'size');
+        this.sizeOptions1 = this.productData?.options.filter(option => option.name === 'color');
+        this.filteredList = this.productData?.base_sku ?? [];
       }
     });
+  }
+  
+  filterByColor(): void {
+    if (this.selectedColor) {
+      this.filteredList = this.productData?.base_sku?.filter(item => item.color_id === this.selectedColor) ?? [];
+    } else {
+      this.filteredList = this.productData?.base_sku ?? [];
+    }
+  }
+  
+  filterBySize(): void {
+    if (this.selectedSize) {
+      this.filteredList = this.filteredList.filter(item => item.size_id === this.selectedSize);
+    }
+  }
+
+  filterByColorOnly(): void {
+    this.selectedSize = null; // Bỏ lọc theo kích thước
+    this.filterByColor();
+  }
+  
+  filterBySizeOnly(): void {
+    this.selectedColor = null; // Bỏ lọc theo màu sắc
+    this.filterBySize();
+  }
+
+  filterList(): void {
+    this.filterByColor();
+    this.filterBySize();   
+  }
+
+  onCategoryChange(): void {
+    this.filterList();
   }
 
 }
